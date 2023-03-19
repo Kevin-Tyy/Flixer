cba4823ee1bf1e1232b8e6161a5b00df5c65ca62
Kevin Tuyizere <tuyizerek27@gmail.com>
2023-03-19 07:39:39 +0200

    further styling

--- src/Components/Pages/404.jsx
+++ src/Components/Pages/404.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Pages.css'
 
-const Page404 = () => {
+const Downloads = () => {
   return (
-    <div style={{textAlign: "center" ,marginTop: 150 , marginBottom : "auto"}}>Page not found</div>
+    <div className='pages'>
+        <h1>Oops! Page not found...</h1>
+        <Link to="/">Go to Home</Link>
+
+    </div>
   )
 }
 
-export default Page404
+export default Downloads
